92014331ca8ce246f729fe07b1a73bb2dee8a1e3
d_ito <ito@gizumo-inc.jp>
2021-12-19 16:03:02 +0900

    add: mac以外のOSで実行した場合の終了処理

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 
+# Mac以外のOSだった場合は処理を中断
+if [ "$(uname)" != "Darwin" ] ; then
+  echo 'Whoops!! Not macOS!!'
+  exit 1
+fi
+
+
+echo ''
+echo 'Start Setup MacOS Settings...'
+echo ''
+
+# Dotfilesの配置
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
